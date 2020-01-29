0a7b7d155763881341877ffd6e7f13d4a66bbeac
abhilash9 <motifadmin@DESKTOP-UAC7QT3.localdomain>
2020-01-29 11:37:34 -0500

    Committing modifications in output and variables files - removing NSG association portions

--- Network-Resources/output.tf
+++ Network-Resources/output.tf
@@ -44,12 +44,19 @@ output "rt_subnets" {
 #---------------------------------------------------------------------------------------------
 # Output from the NSG-Associations Config
 #---------------------------------------------------------------------------------------------
+/*
 output "nsgAssociation_subnet_ids" {
-  value       = [for subnet in data.azurerm_subnet.subnetData: subnet.id] 
+  value       = flatten([for subnet in data.azurerm_subnet.subnetData: subnet.id])
   description = ""
 }
 
 output "nsgAssociation_nsg_ids" {
-  value       = [for nsg in data.azurerm_network_security_group.nsgData: nsg.id]
+  value       = flatten([for nsg in data.azurerm_network_security_group.nsgData: nsg.id])
   description = ""
 }
+
+output "test" {
+  value       = zipmap(flatten([for subnet in data.azurerm_subnet.subnetData: subnet.id]),flatten([for nsg in data.azurerm_network_security_group.nsgData: nsg.id]))
+  description = ""
+}
+*/
--- Network-Resources/variables.tf
+++ Network-Resources/variables.tf
@@ -16,16 +16,3 @@ variable "rt_maps" {
   description = ""
 }
 
-variable "nsgAssociation_maps" {
-  type        = list(any)
-  default     = [
-    {
-      subnet_name = "Adcs-01"
-      vnet_name   = "Hub-01"
-      vnet_rg_name= "RG-Prod"
-      nsg_name    = "NSG-01"
-      nsg_rg_name = "RG-Prod"
-    }
-  ]
-  description = ""
-}
