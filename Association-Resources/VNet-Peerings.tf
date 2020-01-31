13283f121ad0351fca197a1aeab404e1b0cac4d1
abhilash9 <motifadmin@DESKTOP-UAC7QT3.localdomain>
2020-01-31 02:03:00 -0500

    Committing VNET PEERING LOGIC - with count variables

--- Association-Resources/VNet-Peerings.tf
+++ Association-Resources/VNet-Peerings.tf
@@ -1,32 +1,26 @@
 data "azurerm_virtual_network" "vnet1" {
-  provider            = azurerm.crossTenant-ran
-  name                = "${var.vnet_names[0]}"
-  resource_group_name = "${var.rg_names[0]}"
-}
-
-data "azurerm_virtual_network" "vnet2" {
-  provider            = azurerm.crossTenant-nfp
-  name                = "${var.vnet_names[1]}"
-  resource_group_name = "${var.rg_names[1]}"
+  count               = length(var.vnet_names)
+  name                = var.vnet_names[count.index]
+  resource_group_name = element(var.rg_names,count.index)
 }
 
 resource "azurerm_virtual_network_peering" "vnet_peer_1" {
-  provider                     = azurerm.crossTenant-ran
-  name                         = var.peer_names[0]
-  resource_group_name          = var.rg_names[0]
-  virtual_network_name         = var.vnet_names[0]
-  remote_virtual_network_id    = data.azurerm_virtual_network.vnet2.id
+  count                        = length(var.peer_names) == 2 ? 1 : 0
+  name                         = var.peer_names[count.index]
+  resource_group_name          = element(var.rg_names,count.index)
+  virtual_network_name         = element(var.vnet_names,count.index)
+  remote_virtual_network_id    = data.azurerm_virtual_network.vnet1[count.index+1].id
   allow_virtual_network_access = var.allow_virtual_network_access
   allow_forwarded_traffic      = var.allow_forwarded_traffic
   use_remote_gateways          = var.use_remote_gateways
 }
 
 resource "azurerm_virtual_network_peering" "vnet_peer_2" {
-  provider                     = azurerm.crossTenant-nfp
-  name                         = var.peer_names[1]
-  resource_group_name          = var.rg_names[1]
-  virtual_network_name         = var.vnet_names[1]
-  remote_virtual_network_id    = data.azurerm_virtual_network.vnet1.id
+  count                        = length(var.peer_names) == 2 ? 1 : 0
+  name                         = var.peer_names[count.index+1]
+  resource_group_name          = element(var.rg_names,count.index+1)
+  virtual_network_name         = element(var.vnet_names,count.index+1)
+  remote_virtual_network_id    = data.azurerm_virtual_network.vnet1[count.index].id
   allow_virtual_network_access = var.allow_virtual_network_access
   allow_forwarded_traffic      = var.allow_forwarded_traffic
   use_remote_gateways          = var.use_remote_gateways
--- Association-Resources/output.tf
+++ Association-Resources/output.tf
@@ -1,6 +1,7 @@
 #---------------------------------------------------------------------------------------------
 # Output from the VNET-Peerings Config
 #---------------------------------------------------------------------------------------------
+/*
 output "virtualNetworkPeering_peer_ids_1" {
   value       = azurerm_virtual_network_peering.vnet_peer_1.id
   description = ""
@@ -10,3 +11,5 @@ output "virtualNetworkPeering_peer_ids_2" {
   value       = azurerm_virtual_network_peering.vnet_peer_2.id
   description = ""
 }
+
+*/
