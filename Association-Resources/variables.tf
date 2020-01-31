e685bdae269d73dad7ff98344b68860bde8fa728
abhilash9 <motifadmin@DESKTOP-UAC7QT3.localdomain>
2020-01-30 23:09:59 -0500

    Committing the changes to the variables file in Association Resources - Defaults to Null

--- Association-Resources/variables.tf
+++ Association-Resources/variables.tf
@@ -1,35 +1,12 @@
 variable "nsgAssociation_maps" {
   type        = list(any)
-  default     = [
-    {
-      subnet_name = "New-01"
-      vnet_name   = "Hub-01"
-      vnet_rg_name= "RG-Prod"
-      nsg_name    = "NSG-01"
-      nsg_rg_name = "RG-Prod"
-    },
-    {
-      subnet_name = "New-02"
-      vnet_name   = "Hub-01"
-      vnet_rg_name= "RG-Prod"
-      nsg_name    = "NSG-01"
-      nsg_rg_name = "RG-Prod"
-    } 
-  ]
+  default     = []
   description = ""
 }
 
 variable "rtAssociation_maps" {
   type        = list(any)
-  default     = [
-      {
-          rt_name       = "RT-01"
-          rt_rg_name    = "RG-Prod"
-          subnet_name   = "New-01"
-          vnet_name     = "Hub-01"
-          vnet_rg_name  = "RG-Prod"
-      }
-  ]
+  default     = []
   description = ""
 }
 
@@ -38,19 +15,19 @@ variable "rtAssociation_maps" {
 #---------------------------------------------------------------------------------------------
 variable "peer_names" {
   type        = list(any)
-  default     = ["peer1","peer2"]
+  default     = []
   description = ""
 }
 
 variable "rg_names" {
   type        = list(any)
-  default     = ["RG-05","RG-06"]
+  default     = []
   description = ""
 }
 
 variable "vnet_names" {
   type        = list(any)
-  default     = ["Vnet-05","Vnet-06"]
+  default     = []
   description = ""
 }
 
